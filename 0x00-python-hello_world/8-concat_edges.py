926718350b1855288664d5eefaee5dbf23c79d2f
Aystar <gideonayomide75@gmail.com>
2023-03-06 04:00:57 -0800

    print object-oriented programming with Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"{str[39:66]} with {str[0:6]}\n")
+str1 = str[39:66] + str[106:112] + str[:6]
+print(str1)
