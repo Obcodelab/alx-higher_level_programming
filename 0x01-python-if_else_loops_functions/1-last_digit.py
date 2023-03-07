d8d0763ed443fd6198dcb3302ecd9bbb21b8da22
Aystar <gideonayomide75@gmail.com>
2023-03-07 04:04:46 -0800

    prints last digit of variable number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
+
 def checker(num):
     global number
     if num > 5:
         print(f"Last digit of {number} is {num} and is greater than 5")
     elif num < 6 and num != 0:
         print(f"Last digit of {number} is {num} and is less than 6 and not 0")
+
+
 if number < 0:
-    number *= -1
-    last_digit = number % 10
+    last_digit = (number*-1) % 10
     if last_digit > 0:
         last_digit *= -1
         checker(last_digit)
