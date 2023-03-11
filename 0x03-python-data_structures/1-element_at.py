fd2e46904751b7f9ac8686767a64d6b3974c4064
Aystar <gideonayomide75@gmail.com>
2023-03-11 01:18:07 -0800

    retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
-        element = my_list[idx]
-        return ("Element at index {:d} is {}".format(idx, element))
+        return my_list[idx]
