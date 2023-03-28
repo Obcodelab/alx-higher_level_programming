8b3970ee19f8be2e7cdc1482460b4b18489ca195
Aystar <gideonayomide75@gmail.com>
2023-03-28 03:47:32 -0700

    imitates python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,9 +4,10 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
-            result += (a ** b) / i
-        except:
+                raise Exception("Too far")
+            else:
+                result += (a**b)/i
+        except Exception:
             result = b + a
             break
     return result
