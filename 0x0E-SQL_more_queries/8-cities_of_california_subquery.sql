39fd8bf211a4ce34eacfd635c900f746bb5f7b5e
Aystar <gideonayomide75@gmail.com>
2023-05-17 11:31:54 -0700

    a script that lists all the cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,10 +4,10 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 SELECT `id`, `name`
-FROM `hbtn_0d_usa`.`cities`
+FROM `cities`
 WHERE `state_id` IN (
         SELECT `id`
-        FROM `hbtn_0d_usa`.`states`
+        FROM `states`
         WHERE `name` = "California"
     )
 ORDER BY `id`;
