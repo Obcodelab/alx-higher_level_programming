9a958be3c8c8e5a3bc10713869beb8d26e557486
Aystar <gideonayomide75@gmail.com>
2023-05-17 12:14:08 -0700

    a script that lists all shows without a genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,11 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
