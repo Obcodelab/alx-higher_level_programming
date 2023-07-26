1c262da871e8ad84d7395f33f2b651d6737ca1cf
obcodelab <gideonayomide75@gmail.com>
2023-07-26 23:55:42 +0000

    fixed with semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
+const url = process.argv[2];
 const request = require('request');
 
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    let dic = {};
-    let tasks = JSON.parse(body);
-    for (let i in tasks) {
+    const dic = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
       if (tasks[i].completed) {
-	if (dic[tasks[i].userId] === undefined) {
-	  dic[tasks[i].userId] = 1;
-	} else {
-	  dic[tasks[i].userId]++;
-	}
+        if (dic[tasks[i].userId] === undefined) {
+          dic[tasks[i].userId] = 1;
+        } else {
+          dic[tasks[i].userId]++;
+        }
       }
     }
     console.log(dic);
